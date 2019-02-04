ba99aa55f0d7a47803a9a6a5c197361a1fc2d1d6
RC0032171 <RC0032171@CRO-24-01>
2019-02-04 15:38:52 +0000

    changed hello

--- Hello World 1/Form1.cs
+++ Hello World 1/Form1.cs
@@ -19,7 +19,7 @@ namespace Hello_World_1
 
         private void button1_Click(object sender, EventArgs e)
         {
-            MessageBox.Show("Hello World")
+            MessageBox.Show("Hello World");
         }
     }
 }
